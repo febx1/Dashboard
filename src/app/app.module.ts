beab68a031f4ece936089533f57044765197097f
Febin <georgefebin97@gmail.com>
2023-11-02 14:10:54 -0700

    Dashboard
    - Header
    - Left nav design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LeftNavComponent } from './components/left-nav/left-nav.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftNavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
